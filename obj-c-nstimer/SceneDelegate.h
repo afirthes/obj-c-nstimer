d6ebb126c1093a7bea8be39e39f3f4a74c073287
afirthes <afirthes@gmail.com>
2021-07-09 20:27:38 +0300

    Initial Commit

---
+++ obj-c-nstimer/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  obj-c-nstimer
+//
+//  Created by sehio on 09.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ obj-c-nstimer/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  obj-c-nstimer
+//
+//  Created by sehio on 09.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
